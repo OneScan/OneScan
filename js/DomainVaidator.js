50fd1c65b801750a867e65e90efd4fa0bfabbfd8
Jayed Rafi <jayedrafi@outlook.com>
2022-01-22 08:54:20 -0600

    Update DomainVaidator.js

--- js/DomainVaidator.js
+++ js/DomainVaidator.js
@@ -11,17 +11,19 @@ $(document).ready(function(){
 	$("#validator").click(function(){
 		var url = new URL("https://onescan.github.io/sc/");//Server location
 		url += $("#extension").val(); //Extenstion from user input
-		url += "/index.html"; //Smart card availability
+		var console_url = url;
+		url += "/index.html";
 		
 		if(url != ""){
 			$.ajax({
 				url: url,
 				type: 'HEAD',
 				error: function(){
-					$("#console").text("Domain availabe !!!"+"\n"+url);//Location not found means availabe
+				$("#console").text("Domain availabe !!!");//Location not found means availabe
+				$("#console").text(console_url);//Location not found means availabe
 				},
 				success: function(){
-					$("#console").text("Sorry. This domain is already taken. Try again.");//Location found means taken
+					$("console").text("Sorry. This domain is already taken. Try again.");//Location found means taken
 				}
 			});
 		}
