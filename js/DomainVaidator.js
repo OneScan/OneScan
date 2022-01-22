cfa22f30842a7cb4b52d4a82bdc89495aa6191ce
Jayed Rafi <jayedrafi@outlook.com>
2022-01-22 08:19:25 -0600

    Create DomainVaidator.js

---
+++ js/DomainVaidator.js
@@ -0,0 +1,30 @@
+/************************************************************
+ * DomainVaidator.js                                        *
+ *                                                          *
+ * Dev: Jayed Rafi											                    *
+ *															                            *
+ * Properties:												                      *
+ * This program tries to find identical URL is given server.*
+ * *********************************************************/
+$(document).ready(function(){
+	$("#validator").click(function(){
+		var url = new URL("https://onescan.github.io/sc/");//Server location
+		url += $("#extension").val()+"/index.html";
+		
+		if(url != ""){
+			$.ajax({
+				url: url,
+				type: 'HEAD',
+				error: function(){
+				$("#console").text("Domain availabe !!!");//Location not found means availabe
+				},
+				success: function(){
+					$("console").text("Sorry. This domain is already taken. Try again.");//Location found means taken
+				}
+			});
+		}
+		else{
+			$("#console").text("Input not valid !");//Invalid
+		}
+	});
+});
